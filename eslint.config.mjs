b26246a3b9020ff8e7ef242916b893d85c53a6f6
o.s.kalashnikov <askal@ukr.net>
2025-03-17 20:42:31 +0200

    add eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,56 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+import pluginReact from "eslint-plugin-react";
+import airbnbBase from "eslint-config-airbnb/base";
+import airbnbHooks from "eslint-config-airbnb/hooks";
+import {FlatCompat} from "@eslint/eslintrc";
 
 const compat = new FlatCompat({
-  baseDirectory: __dirname,
+  baseDirectory: import.meta.dirname,
 });
 
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  {files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"]},
+  {languageOptions: { globals: globals.browser }},
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  pluginReact.configs.flat.recommended,
+  ...compat.config({
+    extends: ["next/core-web-vitals", "next/typescript"]
+  }),
+  pluginReact.configs.flat['jsx-runtime'], // Add this if you are using React 17+
+  {
+    plugins: {
+      react: pluginReact,
+      hooks: airbnbHooks,
+      // next: nextConfig.configs.recommended.plugins,
+    },
+    rules: {
+      ...airbnbBase.rules,
+      ...pluginReact.configs.recommended.rules,
+      ...airbnbHooks.rules,
+      "react/react-in-jsx-scope": "off", // For Next.js, React is in scope automatically
+      "import/prefer-default-export": "off", // Allow single named exports
+      "@typescript-eslint/no-explicit-any": "off",
+      "no-console": [
+        "error",
+        { "allow": ["info", "error", "trace", "dir"] }
+      ]
+    },
+    languageOptions: {
+      parser: tseslint.parser, // Add TypeScript parser for TypeScript support
+      parserOptions: {
+        ecmaVersion: 2021,
+        tsconfigRootDir: import.meta.dirname,
+        sourceType: "module",
+      },
+    },
+    settings: {
+      next: {
+        rootDir: ".", // Specify your Next.js root directory if needed
+      },
+    },
+  },
 ];
-
-export default eslintConfig;
