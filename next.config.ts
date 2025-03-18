81d9d35fc6462c25a0204d3117d536521690046f
o.s.kalashnikov <askal@ukr.net>
2025-03-18 16:56:02 +0200

    feature: add styles, add scss variables

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  sassOptions: {
+    prependData: `@import './src/shared/assets/styles/config.scss';`
+  },
 };
 
 export default nextConfig;
