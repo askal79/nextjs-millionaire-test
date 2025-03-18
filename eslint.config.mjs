fd888a193af4384bd2ac0ea7cd283c9002e826dc
o.s.kalashnikov <askal@ukr.net>
2025-03-18 10:47:58 +0200

    update lint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,61 +1,47 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-import tseslint from "typescript-eslint";
-import pluginReact from "eslint-plugin-react";
-import airbnbBase from "eslint-config-airbnb/base";
-import airbnbHooks from "eslint-config-airbnb/hooks";
-import {FlatCompat} from "@eslint/eslintrc";
+/* eslint-disable */
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import pluginReact from 'eslint-plugin-react';
+import airbnbBase from 'eslint-config-airbnb/base';
+import airbnbHooks from 'eslint-config-airbnb/hooks';
+import { FlatCompat } from '@eslint/eslintrc';
 
 const compat = new FlatCompat({
   baseDirectory: import.meta.dirname,
 });
 
 /** @type {import('eslint').Linter.Config[]} */
-// eslint-disable-next-line import/no-anonymous-default-export
 export default [
-  {files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"]},
-  {languageOptions: { globals: globals.browser }},
+  { files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'] },
+  { ignores: ['.prettierrc.mjs', '.lintstagedrc.mjs', '.github/', 'next.config.ts'] },
+  { languageOptions: { globals: globals.browser } },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   pluginReact.configs.flat.recommended,
   ...compat.config({
-    ignorePatterns: [".prettierrc.mjs", ".lintstagedrc.mjs"],
-    extends: ["next/core-web-vitals", "next/typescript", "next", "prettier"]
-  }),
-  pluginReact.configs.flat['jsx-runtime'], // Add this if you are using React 17+
-  {
-    plugins: {
-      react: pluginReact,
-      hooks: airbnbHooks,
-      // next: nextConfig.configs.recommended.plugins,
-    },
+    extends: ['prettier', 'plugin:react/recommended', 'airbnb/hooks', 'plugin:@typescript-eslint/recommended', 'airbnb/base', 'next/core-web-vitals', 'next/typescript'],
     rules: {
       ...airbnbBase.rules,
       ...pluginReact.configs.recommended.rules,
       ...airbnbHooks.rules,
-      "react/react-in-jsx-scope": "off", // For Next.js, React is in scope automatically
-      "import/prefer-default-export": "off", // Allow single named exports
-      "@typescript-eslint/no-explicit-any": "off",
-      "no-console": [
-        "error",
-        { "allow": ["info", "error", "trace", "dir"] }
-      ]
-    },
-    languageOptions: {
-      parser: tseslint.parser, // Add TypeScript parser for TypeScript support
-      parserOptions: {
-        ecmaVersion: 2021,
-        tsconfigRootDir: import.meta.dirname,
-        sourceType: "module",
-      },
+      'react/react-in-jsx-scope': 'off', // For Next.js, React is in scope automatically
+      'import/prefer-default-export': 'off', // Allow single named exports
+      '@typescript-eslint/no-explicit-any': 'off',
+      'no-console': ['error', { allow: ['info', 'error', 'trace', 'dir'] }],
     },
     settings: {
-        react: {
-          version: '19.0'
-        },
+      react: {
+        version: '19.0',
+      },
       next: {
-        rootDir: ".", // Specify your Next.js root directory if needed
+        rootDir: '.', // Specify your Next.js root directory if needed
       },
     },
-  },
+    parser: '@typescript-eslint/parser', // Add TypeScript parser for TypeScript support
+    parserOptions: {
+      ecmaVersion: 2021,
+      sourceType: 'module',
+    },
+  }),
 ];
